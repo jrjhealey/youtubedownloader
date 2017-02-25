422637c51b492ccbca09cec69681951efd7fb7e8
Joe Healey <jrj.healey@gmail.com>
2017-02-25 17:20:00 +0000

    Update rundownloader.bat

--- rundownloader.bat
+++ rundownloader.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Batch script that invokes youtube-dl to download and convert files from youtube in to audio.
 :: Replace "PASTE_YOUR_URL_HERE" with your desired youtube link, and save the file. Then simply,
-:: call this file directly: '> rundownloader.bat
+:: call this file directly: '> rundownloader.bat'
 
 youtube-dl.exe PASTE_YOUR_URL_HERE -i --yes-playlist -x --audio-format "mp3" --prefer-ffmpeg --yes-playlist --ffmpeg-location .\ffmpeg\bin\ffmpeg.exe --output ".\results\%%(title)s.%%(ext)s"
