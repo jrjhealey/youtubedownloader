1fe7f5f2ba3fd8ebf0ab62f02a6c165ea7b87841
Joe Healey <jrj.healey@gmail.com>
2017-03-26 17:46:10 +0100

    modified to accept url from commandline

--- rundownloader.bat
+++ rundownloader.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Batch script that invokes youtube-dl to download and convert files from youtube in to audio.
 :: Replace "PASTE_YOUR_URL_HERE" with your desired youtube link, and save the file. Then simply,
-:: call this file directly: '> rundownloader.bat'
+:: call this file directly: '> rundownloader.bat https://www.youtube.com/someyoutubelink'
 
-youtube-dl.exe PASTE_YOUR_URL_HERE -i --yes-playlist -x --audio-format "mp3" --prefer-ffmpeg --yes-playlist --ffmpeg-location .\ffmpeg\bin\ffmpeg.exe --output ".\results\%%(title)s.%%(ext)s"
+youtube-dl.exe %2 -i --yes-playlist -x --audio-format "mp3" --prefer-ffmpeg --yes-playlist --ffmpeg-location .\ffmpeg\bin\ffmpeg.exe --output ".\results\%%(title)s.%%(ext)s"
